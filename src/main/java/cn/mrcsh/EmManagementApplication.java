f482f31102871de1dc9caf3aa3d8cde77751f91b
崔世豪 <virus_cui@outlook.com>
2023-02-10 19:05:15 +0800

    首次提交

---
+++ src/main/java/cn/mrcsh/EmManagementApplication.java
@@ -0,0 +1,13 @@
+package cn.mrcsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmManagementApplication.class, args);
+    }
+
+}
