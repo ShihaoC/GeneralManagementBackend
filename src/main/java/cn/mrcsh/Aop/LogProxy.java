6af433c16f7914a8c05c6d36a0f5af346dddba57
崔世豪 <virus_cui@outlook.com>
2023-03-02 17:45:14 +0800

    添加日志注解

--- src/main/java/cn/mrcsh/Aop/LogProxy.java
+++ src/main/java/cn/mrcsh/Aop/LogProxy.java
@@ -1,7 +1,9 @@
 package cn.mrcsh.Aop;
 
 import cn.mrcsh.Annotations.Log;
+import cn.mrcsh.Util.JwtUtil;
 import cn.mrcsh.Util.RedisUtil;
+import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
@@ -36,16 +38,22 @@ public class LogProxy {
         //***********************************************************************************
         String api = method.getAnnotation(Log.class).api(); // api名称
         String module = method.getAnnotation(Log.class).module(); // 模块名称
+        String username = ""; // 用户名
         Long currentTime = System.currentTimeMillis(); // 时间戳
+        String Type = request.getMethod(); // 请求方式
         String token = request.getHeader("authorization"); // token 用JwtUtil解析token
+        String ip = getIP(request); // 操作地址
         redisUtil.set(currentTime+":["+module+"("+api+")]",request.getHeader("authorization"));
         log.info(request.getHeader("authorization"));
-
-
-
-
-
-
+        Claims claims = null;
+        try {
+            claims = JwtUtil.checkToken(token);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        if(claims != null){
+            username = claims.getSubject();
+        }
 
 
 
@@ -53,5 +61,22 @@ public class LogProxy {
         //***********************************************************************************
         res = point.proceed();
         return res;
+    }
+
+    public String getIP(HttpServletRequest request){
+        String ip = ""; // 操作地址
+        if(request.getHeader("x-forwarded-for") != null){
+            ip = request.getHeader("x-forwarded-for");
+        }else if(request.getHeader("Proxy-Client-IP") != null){
+            ip = request.getHeader("Proxy-Client-IP");
+        }else if(request.getHeader("WL-Proxy-Client-IP") != null){
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }else if(request.getRemoteAddr() != null){
+            ip = request.getRemoteAddr();
+        }
+        if(ip.equals("127.0.0.1") || ip.equals("0:0:0:0:0:0:0:1") || ip.contains("172")){
+            ip = null;
+        }
+        return ip;
     }
 }
